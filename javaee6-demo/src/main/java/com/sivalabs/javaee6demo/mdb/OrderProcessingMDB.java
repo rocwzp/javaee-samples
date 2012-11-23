5f27dc415da83504ad16c3571d6c13b9772b3a41
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2012-11-23 17:41:37 +0530

    Added JMS msg properties print utility methods.

--- javaee6-demo/src/main/java/com/sivalabs/javaee6demo/mdb/OrderProcessingMDB.java
+++ javaee6-demo/src/main/java/com/sivalabs/javaee6demo/mdb/OrderProcessingMDB.java
@@ -1,7 +1,10 @@
 package com.sivalabs.javaee6demo.mdb;
 
+import java.util.Enumeration;
+
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -28,11 +31,63 @@ public class OrderProcessingMDB implements MessageListener
 		try
 		{
 			content = ((TextMessage)msg).getText();
+			System.err.println("Received Message :"+content);
+			printJmsMessageHeaders(msg);
+			
 		} catch (JMSException e)
 		{
 			e.printStackTrace();
 		}
-		System.err.println("Received Message :"+content);
+		
+	}
+	
+	public static void printJmsMessageHeaders(Message msg)
+	{
+		try
+		{
+			String jmsCorrelationID = msg.getJMSCorrelationID();
+			System.out.println("jmsCorrelationID : "+jmsCorrelationID);
+			String jmsMessageID = msg.getJMSMessageID();
+			System.out.println("jmsMessageID : "+jmsMessageID);
+			long jmsExpiration = msg.getJMSExpiration();
+			System.out.println("jmsExpiration : "+jmsExpiration);
+			int jmsPriority = msg.getJMSPriority();
+			System.out.println("jmsPriority : "+jmsPriority);
+			int jmsDeliveryMode = msg.getJMSDeliveryMode();
+			System.out.println("jmsDeliveryMode : "+jmsDeliveryMode);
+			Destination jmsDestination = msg.getJMSDestination();
+			System.out.println("jmsDestination : "+jmsDestination);
+			boolean jmsRedelivered = msg.getJMSRedelivered();
+			System.out.println("jmsRedelivered : "+jmsRedelivered);
+			Destination jmsReplyTo = msg.getJMSReplyTo();
+			System.out.println("jmsReplyTo : "+jmsReplyTo);
+			long jmsTimestamp = msg.getJMSTimestamp();
+			System.out.println("jmsTimestamp : "+jmsTimestamp);
+			String jmsType = msg.getJMSType();
+			System.out.println("jmsType : "+jmsType);
+			
+		} catch (JMSException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static void printMessageProperties(Message msg)
+	{
+		try
+		{
+			Enumeration propertyNames = msg.getPropertyNames();
+			while(propertyNames.hasMoreElements()) 
+			{
+				String name = (String)propertyNames.nextElement();
+				Object value = msg.getObjectProperty(name);
+				System.out.println("\nname="+value);
+			}
+		} catch (JMSException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
