0d8fad2b269666dc00b6292a4ca4e261ec833a81
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2013-07-30 18:26:15 +0530

    Added registration functionality.

--- tweeter/src/main/java/com/sivalabs/tweeter/controllers/UserBean.java
+++ tweeter/src/main/java/com/sivalabs/tweeter/controllers/UserBean.java
@@ -16,7 +16,7 @@ import com.sivalabs.tweeter.entities.User;
 
 @Named
 @SessionScoped
-public class LoginBean implements Serializable
+public class UserBean implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	private Logger logger = LoggerFactory.getLogger(getClass());
@@ -25,6 +25,7 @@ public class LoginBean implements Serializable
 	private UserEJB userEJB;
 	
 	private User loginUser;
+	private User registrationUser;
 	private String email;
 	private String password;
 	
@@ -41,12 +42,41 @@ public class LoginBean implements Serializable
     	return view;
     }
 
+    public String register()
+    {
+    	String view="registration";
+    	User user = userEJB.createUser(registrationUser);
+    	if(user != null){
+    		view = "index.jsf?faces-redirect=true";
+    		registrationUser = null;
+    		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+    		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successfully Registered", null));
+    	} else {
+    		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid UserName and Password", null));
+    	}
+    	return view;
+    }
+    
 	public User getLoginUser() {
 		return loginUser;
 	}
 	public void setLoginUser(User loginUser) {
 		this.loginUser = loginUser;
 	}
+	
+	public User getRegistrationUser()
+	{
+		if(registrationUser == null){
+			registrationUser = new User();
+		}
+		return registrationUser;
+	}
+
+	public void setRegistrationUser(User registrationUser)
+	{
+		this.registrationUser = registrationUser;
+	}
+
 	public String getEmail() {
 		return email;
 	}
